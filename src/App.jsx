cd711c74ea5988c0ebdf5799a0f9466c1323bf66
Brian Rahadi <brian.rahadi@gmail.com>
2024-08-17 09:23:42 -0700

    Init tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { VoiceWidget } from "./components/VoiceWidget";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App text-xl">
       <h1>Today's Attendees</h1>
       <VoiceWidget />
     </div>
