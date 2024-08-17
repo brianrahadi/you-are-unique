0a05fc0b07c434530ad9d5e29f331c58a5081bc5
Brian Rahadi <brian.rahadi@gmail.com>
2024-08-17 12:45:14 -0700

    Add post-data hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState, useEffect } from "react";
 import { VoiceWidget } from "./components/VoiceWidget";
 import useFetchData from './hooks/use-fetch-data'
+import postData from './hooks/post-data'
 
 function App() {
   const { data, loading } = useFetchData();
+  const { success } = postData("wow123");
   return (
     <div className="App text-xl flex justify-center content-center mx-auto">
       <div className="">
       <h1>Today's Attendees</h1>
-      {JSON.stringify(data)}
+      {!loading && JSON.stringify(data)}
       <VoiceWidget />
+      <p>Post Data: {success ? 1 : 0}</p>
       </div>
-
     </div>
   );
 }
--- server/routes/root.js
+++ server/routes/root.js
@@ -4,4 +4,9 @@ module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
+
+  fastify.post('/', async function (request, reply) {
+    console.log("HAHAH", request)
+    return { root: 123 }
+  })
 }
