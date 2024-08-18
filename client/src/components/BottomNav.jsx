909806b1d71e17f9a96bac5c3a96c815cdaf32c0
Sacha <150120887+c-est-sa@users.noreply.github.com>
2024-08-17 18:26:05 -0700

    style: nav bar

--- client/src/components/BottomNav.jsx
+++ client/src/components/BottomNav.jsx
@@ -4,9 +4,19 @@ import { Link } from "react-router-dom";
 const BottomNav = () => {
   return (
     <nav className="fixed bottom-0 bg-slate-300 w-full h-12">
-      <ul className="">
-        <Link to="/all">All</Link>
-        <Link to="/dashboard">Today</Link>
+      <ul className="h-full">
+        <Link
+          to="/all"
+          className="inline-block w-32 text-center text-black hover:bg-slate-50 leading-10"
+        >
+          All
+        </Link>
+        <Link
+          to="/dashboard"
+          className="inline-block w-32 text-center text-black hover:bg-slate-50 leading-10"
+        >
+          Today
+        </Link>
       </ul>
     </nav>
   );
