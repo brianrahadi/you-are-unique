eff5aee29f0eeb7c409f8700dd964b4637104e34
Brian Rahadi <brian.rahadi@gmail.com>
2024-08-18 14:35:44 -0700

    Add static notes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -27,6 +27,26 @@ const Profile = () => {
           <p className="text-gray-500">Last Visited</p>
           <p className="text-gray-500 col-span-2">{lastVisited}</p>
         </div>
+
+        <div className="grid grid-cols-3 pt-4">
+          <p className="text-gray-500">Times Visited</p>
+          <p className="text-gray-500 col-span-2">{timesVisited}</p>
+        </div>
+
+        <div className="grid text-center pt-4">
+          <p className="text-gray-500">Notes</p>
+        </div>
+        <ul className="text-gray-500 col-span-2">
+          {[
+            "Loves cookie so much",
+            "Has 2 lovely kids",
+            "Usually jogs in the morning",
+            "Hates pancake with syrup",
+            "Born and Raised in small town in Russia",
+            "Almost got into an accident with a bear"
+          ].filter(() => Math.random() > 0.6)
+            .map(text => <li className="text-left text-gray-500 list-disc">{text}</li>)}
+        </ul>
       </div>
     </div>
   );
